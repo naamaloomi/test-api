43d5f928761add058bb5ada01e213a2bcf558f9b
Amir Sadeghin <amir.sadeghin@gmail.com>
2017-02-21 21:28:50 +0100

    Initial commit.

---
+++ TestApi/Models/AssetDao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApi.Models
+{
+    public class AssetDao
+    {
+        private Dictionary<string, Asset> database;
+
+        public Nullable<Asset> getAsset(string serialNumber)
+        {
+            Asset asset = null;
+            database.TryGetValue(serialNumber, out asset);
+            return asset;
+        }
+    }
+}
